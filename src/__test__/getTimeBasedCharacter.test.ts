21f2e118fa6f5bd5f875b0393f5972f03b52b3e1
chinsun9 <chin_sung@naver.com>
2021-09-25 20:22:51 +0900

    test: BASE_TIME으로부터 24, 207, 208, 209시간 후 테스트

--- src/__test__/getTimeBasedCharacter.test.ts
+++ src/__test__/getTimeBasedCharacter.test.ts
@@ -1,9 +1,10 @@
 /* eslint-disable no-plusplus */
 import { BASE_TIME } from '../baseTime';
 import { getTimeBasedCharacter } from '../getTimeBasedCharacter';
+import anthemCharacters from '../anthem.json';
 
+const oneHour = 3600 * 1000;
 describe('BASE_TIEM으로부터 연속된 테스트', () => {
-  const oneHour = 3600 * 1000;
   let seq = 0;
   it('should be 동', () => {
     expect(getTimeBasedCharacter(BASE_TIME + oneHour * seq++)).toEqual('동');
@@ -28,5 +29,34 @@ describe('BASE_TIEM으로부터 연속된 테스트', () => {
   });
   it('should be 이', () => {
     expect(getTimeBasedCharacter(BASE_TIME + oneHour * seq++)).toEqual('이');
+  });
+});
+
+describe('BASE_TIME으로부터 1일 후 테스트', () => {
+  const er = anthemCharacters[24];
+  it(`should be ${er}`, () => {
+    expect(getTimeBasedCharacter(BASE_TIME + oneHour * 24)).toEqual(er);
+  });
+});
+
+describe('BASE_TIME으로부터 208시간 전후 테스트', () => {
+  it(`should be 동`, () => {
+    expect(
+      getTimeBasedCharacter(BASE_TIME + oneHour * anthemCharacters.length),
+    ).toEqual('동');
+  });
+  it(`should be 해`, () => {
+    expect(
+      getTimeBasedCharacter(
+        BASE_TIME + oneHour * (anthemCharacters.length + 1),
+      ),
+    ).toEqual('해');
+  });
+  it(`should be 세`, () => {
+    expect(
+      getTimeBasedCharacter(
+        BASE_TIME + oneHour * (anthemCharacters.length - 1),
+      ),
+    ).toEqual('세');
   });
 });
